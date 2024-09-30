39a0fed8e22f426082ac9e393009fe88e55c159e
nurs_996 <98372@gamil.com>
2024-09-30 14:02:45 +0600

    fixing bug left

--- src/components/GaveUp.jsx
+++ src/components/GaveUp.jsx
@@ -53,7 +53,7 @@ const ContentWrapper = styled(Box)(({ theme }) => ({
   [theme.breakpoints.down('sm')]: {
     width: '100wh',
     padding: '0 10px',
-    marginLeft: "105px"
+    marginLeft: "100px"
   },
 }));
 const SearchContainer = styled(Box)(({ theme }) => ({
