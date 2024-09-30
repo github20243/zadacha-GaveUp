874818d1047661a578243464c2b31000ad7bc5c3
nurs_996 <98372@gamil.com>
2024-09-30 14:04:26 +0600

    left

--- src/components/GaveUp.jsx
+++ src/components/GaveUp.jsx
@@ -53,7 +53,7 @@ const ContentWrapper = styled(Box)(({ theme }) => ({
   [theme.breakpoints.down('sm')]: {
     width: '100wh',
     padding: '0 10px',
-    marginLeft: "100px"
+    marginLeft: "90px"
   },
 }));
 const SearchContainer = styled(Box)(({ theme }) => ({
